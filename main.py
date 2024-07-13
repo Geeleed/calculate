84079059f1777f08e82798af917202a06a51272e
Geeleed <surasak.kaewpho@gmail.com>
2024-07-13 19:43:32 +0700

    add index.html

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-# uvicorn main:app --port 8000 --reload
+# uvicorn main:app --port 8000 --reload --host 0.0.0.0
 
 from declareAppApi import *
 
 @app.get("/")
 def root():
-    return "connection ok. You can use api in /docs page."
+    # return "connection ok. You can use api in /docs page."
+    return FileResponse("index.html")
 
 from apis.calc import *
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app)
+    # uvicorn.run(app)
+    uvicorn.run(app,host="0.0.0.0")
